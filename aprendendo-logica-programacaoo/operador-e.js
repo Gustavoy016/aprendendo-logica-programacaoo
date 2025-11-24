d7355b805926e3519e4496513c46f62c0782b364
Gustavoy016 <gustavoyamaki18@gmail.com>
2025-11-24 20:18:53 -0300

    aprendendo

---
+++ aprendendo-logica-programacaoo/operador-e.js
@@ -0,0 +1,8 @@
+let temIngresso = true
+let maiorIdade = false
+
+if(temIngresso && maiorIdade) {
+    alert("Entrada permitida!")
+}else {
+    alert("Entrada negada!")
+}
---
+++ aprendendo-logica-programacaoo/operadorOU.js
@@ -0,0 +1,9 @@
+let temIngresso = true
+let nomeNaLista = true
+
+if(temIngresso || nomeNaLista) {
+    alert("Entrada permitida!")
+}else {
+    alert("Entrada negada!")
+}
+
